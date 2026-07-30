dc623a18ea2d25af30fd399d1c5c94fad41b11dc
Dhanajay1813 <dhananjaykakad123@gmail.com>
2026-07-30 16:44:19 +0530

    Add manual content for bel-dit

--- src/content/contentRegistry.js
+++ src/content/contentRegistry.js
@@ -1,29 +1,24 @@
-import acsManualContent from './manuals/acs/manualContent.json';
-import bel_adaManualContent from './manuals/bel-ada/manualContent.json';
-
 import manualIndex from './manualIndex.json';
+import bel_ditManualContent from './manuals/bel-dit/manualContent.json';
 
 export const submittedManualIndex = manualIndex;
 
 export const submittedManuals = {
-  'acs': acsManualContent.manuals['acs'],
-  'bel-ada': bel_adaManualContent.manuals['bel-ada'],
+  'bel-dit': bel_ditManualContent.manuals['bel-dit'],
 };
 
 export const submittedManualAssets = {
-  'acs/images/test/objective/71XA_N8Xj1L._SX522_.jpg': require('./manuals/acs/images/test/objective/71XA_N8Xj1L._SX522_.webp'),
-  'acs/images/test/objective/71XA_N8Xj1L._SX522_.webp': require('./manuals/acs/images/test/objective/71XA_N8Xj1L._SX522_.webp'),
-  'bel-ada/images/exp1/observation/01_Screenshot_from_2026-07-18_12-40-43.png': require('./manuals/bel-ada/images/exp1/observation/01_Screenshot_from_2026-07-18_12-40-43.webp'),
-  'bel-ada/images/exp1/observation/01_Screenshot_from_2026-07-18_12-40-43.webp': require('./manuals/bel-ada/images/exp1/observation/01_Screenshot_from_2026-07-18_12-40-43.webp'),
-  'bel-ada/images/exp1/observation/02_Screenshot_from_2026-07-18_12-40-50.png': require('./manuals/bel-ada/images/exp1/observation/02_Screenshot_from_2026-07-18_12-40-50.webp'),
-  'bel-ada/images/exp1/observation/02_Screenshot_from_2026-07-18_12-40-50.webp': require('./manuals/bel-ada/images/exp1/observation/02_Screenshot_from_2026-07-18_12-40-50.webp'),
-  'bel-ada/images/exp1/observation/03_Screenshot_from_2026-07-18_12-41-04.png': require('./manuals/bel-ada/images/exp1/observation/03_Screenshot_from_2026-07-18_12-41-04.webp'),
-  'bel-ada/images/exp1/observation/03_Screenshot_from_2026-07-18_12-41-04.webp': require('./manuals/bel-ada/images/exp1/observation/03_Screenshot_from_2026-07-18_12-41-04.webp'),
-  'bel-ada/images/exp1/technicalData/circuitDiagram/Screenshot_from_2026-07-18_11-20-10.png': require('./manuals/bel-ada/images/exp1/technicalData/circuitDiagram/Screenshot_from_2026-07-18_11-20-10.webp'),
-  'bel-ada/images/exp1/technicalData/circuitDiagram/Screenshot_from_2026-07-18_11-20-10.webp': require('./manuals/bel-ada/images/exp1/technicalData/circuitDiagram/Screenshot_from_2026-07-18_11-20-10.webp'),
+  'bel-dit/images/exp2/procedure/01_1_realization_of_all_basic_gates_using_nor_gate.webp': require('./manuals/bel-dit/images/exp2/procedure/01_1_realization_of_all_basic_gates_using_nor_gate.webp'),
+  'bel-dit/images/exp2/procedure/02_image_-_2.webp': require('./manuals/bel-dit/images/exp2/procedure/02_image_-_2.webp'),
+  'bel-dit/images/exp2/procedure/03_2_realization_of_all_basic_gates_using_nand_gate.webp': require('./manuals/bel-dit/images/exp2/procedure/03_2_realization_of_all_basic_gates_using_nand_gate.webp'),
+  'bel-dit/images/exp2/procedure/04_image_-4.webp': require('./manuals/bel-dit/images/exp2/procedure/04_image_-4.webp'),
+  'bel-dit/images/exp2/procedure/05_3_realization_of_basic_exor_gate_using_and_nor_gate.webp': require('./manuals/bel-dit/images/exp2/procedure/05_3_realization_of_basic_exor_gate_using_and_nor_gate.webp'),
+  'bel-dit/images/exp2/result/result.webp': require('./manuals/bel-dit/images/exp2/result/result.webp'),
+  'bel-dit/images/exp2/theory/01_image_-1.webp': require('./manuals/bel-dit/images/exp2/theory/01_image_-1.webp'),
+  'bel-dit/images/exp2/theory/02_image_-2.webp': require('./manuals/bel-dit/images/exp2/theory/02_image_-2.webp'),
+  'bel-dit/images/exp2/theory/03_image_-3.webp': require('./manuals/bel-dit/images/exp2/theory/03_image_-3.webp'),
 };
 
 export const getSubmittedManualImageSource = (manualId, imageFile) => (
   submittedManualAssets[`${manualId}/${imageFile}`] || null
 );
-
